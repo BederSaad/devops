4646d194250da04c9710ee3ece19aa7f5bd8ef9f
Beder Saad <153820796+BederSaad@users.noreply.github.com>
2026-03-02 10:04:33 +0100

    Update StudentManagementApplicationTests.java

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -1,5 +1,6 @@
 package tn.esprit.studentmanagement;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(true);
     }
 
 }
